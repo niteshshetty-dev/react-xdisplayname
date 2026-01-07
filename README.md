6ae9fa02fe9127a8d5a97327958153d0d11f0ae7
nthshtty <nthsh6shtty@gmail.com>
2026-01-07 10:58:41 +0530

    Form component is built as per requirement

---
+++ README.md
@@ -0,0 +1,12 @@
+Application Requirements:
+Your application must look as follows upon the initial render:
+
+After entering first and last name, and clicking on the submit button, you must get the full name displayed as follows:
+
+You must make the input fields such that the user is unable to submit without filling in both the fields.
+
+NOTE:
+
+You must make this form using the form element.
+You must create the button using button element.
+You must use the input fields using the input element.
---
+++ src/components/Form.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import styles from "./Form.module.css";
+
+export default function Form() {
+  const [name, setName] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const [displayName, setDisplayName] = useState("");
+
+  const handleChange = (e) => {
+    setName({
+      ...name,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setDisplayName(name.firstName + " " + name.lastName);
+    setName({
+      firstName: "",
+      lastName: "",
+    });
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        name="formData"
+        className={styles.form}
+      >
+        <h1>Full Name display</h1>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            name="firstName"
+            value={name.firstName}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            value={name.lastName}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+        <div>{displayName && <span>Full Name: {displayName}</span>}</div>
+      </form>
+    </div>
+  );
+}
